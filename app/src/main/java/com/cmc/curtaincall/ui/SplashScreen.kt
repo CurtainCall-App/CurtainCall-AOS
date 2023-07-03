8a44df79285498f67fd30515aced5a44a098b16c
ows3090 <ows3090@naver.com>
2023-07-03 15:14:30 +0900

    [feat] #12 Add empty screen for login, signup, splash

---
+++ app/src/main/java/com/cmc/curtaincall/ui/SplashScreen.kt
@@ -0,0 +1,9 @@
+package com.cmc.curtaincall.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SplashScreen() {
+    Text(text = "splash")
+}
---
+++ feature/auth/src/main/java/com/cmc/curtaincall/feature/auth/ui/SignUpScreen.kt
@@ -0,0 +1,9 @@
+package com.cmc.curtaincall.feature.auth.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SignUpScreen() {
+    Text(text = "signup")
+}
