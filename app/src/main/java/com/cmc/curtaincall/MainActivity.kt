8b4b2211a40d9dc7af19dc657707501062ef3e04
ows3090 <ows3090@naver.com>
2023-07-03 15:21:32 +0900

    [feat] #12 Implement MainActivity to start AppNavHost

--- app/src/main/java/com/cmc/curtaincall/MainActivity.kt
+++ app/src/main/java/com/cmc/curtaincall/MainActivity.kt
@@ -6,41 +6,28 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.cmc.curtaincall.navigation.AppNavHost
 import com.cmc.curtaincall.ui.theme.CurtainCallTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CurtainCallTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            CurtainCallApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
+private fun CurtainCallApp() {
     CurtainCallTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            AppNavHost()
+        }
     }
 }
