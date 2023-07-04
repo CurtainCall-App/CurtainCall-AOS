cb8552291d50e5d546067910d0ec947b72713439
ows3090 <ows3090@naver.com>
2023-07-04 14:17:32 +0900

    [feat] #12 Create basic navigate screen in performance

---
+++ feature/performance/src/main/java/com/cmc/curtaincall/feature/performance/ui/PerformanceReviewScreen.kt
@@ -0,0 +1,15 @@
+package com.cmc.curtaincall.feature.performance.ui
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+internal fun PerformanceReviewScreen() {
+    Text(
+        text = "performance review",
+        modifier = Modifier.size(150.dp, 100.dp)
+    )
+}
