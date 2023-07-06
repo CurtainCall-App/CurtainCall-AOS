9cc72c63fca7711f8808ca975865b363bab2569d
ows3090 <ows3090@naver.com>
2023-07-06 17:12:23 +0900

    [feat] #9 Init kakao sdk setting

--- app/src/main/java/com/cmc/curtaincall/CurtainCallApplication.kt
+++ app/src/main/java/com/cmc/curtaincall/CurtainCallApplication.kt
@@ -1,11 +1,13 @@
 package com.cmc.curtaincall
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import timber.log.Timber
 
 class CurtainCallApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        KakaoSdk.init(this, BuildConfig.KAKAO_APP_KEY)
     }
 }
