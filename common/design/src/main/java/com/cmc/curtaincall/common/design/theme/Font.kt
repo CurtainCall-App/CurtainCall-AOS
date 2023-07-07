86bd4b365c14c2be0307f39ce401225ad802ad42
ows3090 <ows3090@naver.com>
2023-07-07 17:40:25 +0900

    [feat] #20 Add spoqahansansneo fontfamily for application

---
+++ common/design/src/main/java/com/cmc/curtaincall/common/design/theme/Font.kt
@@ -0,0 +1,15 @@
+package com.cmc.curtaincall.common.design.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.cmc.curtaincall.common.design.R
+
+val spoqahansanseeo = FontFamily(
+    Font(R.font.spoqahansansneo_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.spoqahansansneo_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.spoqahansansneo_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.spoqahansansneo_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.spoqahansanseeo_thin, FontWeight.Thin, FontStyle.Normal)
+)
