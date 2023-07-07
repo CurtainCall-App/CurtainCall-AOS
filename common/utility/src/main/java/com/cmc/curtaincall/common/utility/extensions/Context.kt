962d44b9844a2436ed1f32ad7e2d16bab1d75be2
ows3090 <ows3090@naver.com>
2023-07-07 15:13:01 +0900

    [build] #10 Revise ktlint and detekt error

--- common/utility/src/main/java/com/cmc/curtaincall/common/utility/extensions/Context.kt
+++ common/utility/src/main/java/com/cmc/curtaincall/common/utility/extensions/Context.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Context
 import android.content.ContextWrapper
 
-
 fun Context.findActivity(): Activity {
     var context = this
     while (context is ContextWrapper) {
