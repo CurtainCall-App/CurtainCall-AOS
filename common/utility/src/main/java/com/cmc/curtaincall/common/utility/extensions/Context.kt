9ddb2b13448a186091568a928b2c6774f18dd5e7
ows3090 <ows3090@naver.com>
2023-07-07 15:08:23 +0900

    [feat] #10 Create Context Extension to get activity

---
+++ common/utility/src/main/java/com/cmc/curtaincall/common/utility/extensions/Context.kt
@@ -0,0 +1,15 @@
+package com.cmc.curtaincall.common.utility.extensions
+
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+
+
+fun Context.findActivity(): Activity {
+    var context = this
+    while (context is ContextWrapper) {
+        if (context is Activity) return context
+        context = context.baseContext
+    }
+    throw IllegalStateException()
+}
