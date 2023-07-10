641eb261733c77da6706ee0879757a13f2f5a321
ows3090 <ows3090@naver.com>
2023-07-10 10:48:05 +0900

    [feat] #25 Install SplashScreen API and change status bar color in Splash Screen

--- app/src/main/java/com/cmc/curtaincall/MainActivity.kt
+++ app/src/main/java/com/cmc/curtaincall/MainActivity.kt
@@ -8,11 +8,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.cmc.curtaincall.common.design.theme.CurtainCallTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
         setContent {
             CurtainCallApp()
         }
--- app/src/main/java/com/cmc/curtaincall/ui/SplashScreen.kt
+++ app/src/main/java/com/cmc/curtaincall/ui/SplashScreen.kt
@@ -1,41 +1,25 @@
 package com.cmc.curtaincall.ui
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import com.cmc.curtaincall.common.design.R
 import com.cmc.curtaincall.common.design.theme.Gunmetal
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(onNavigateOnBoarding: () -> Unit) {
     val systemUiController = rememberSystemUiController()
-    systemUiController.setSystemBarsColor(Gunmetal)
+    systemUiController.setStatusBarColor(Gunmetal)
 
-    LaunchedEffect(systemUiController) {
-        delay(1000)
+    LaunchedEffect(true) {
         onNavigateOnBoarding()
     }
-
-    Column(
-        modifier = Modifier
+    Spacer(
+        Modifier
             .fillMaxSize()
-            .background(Gunmetal),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterResource(R.drawable.ic_logo),
-            contentDescription = stringResource(R.string.splash_logo_image_description)
-        )
-    }
+            .background(Gunmetal)
+    )
 }
