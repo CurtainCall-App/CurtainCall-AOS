ca031d585599c0be0b0fc452fca36e3987e46fc3
ows3090 <ows3090@naver.com>
2023-07-13 20:59:23 +0900

    [feat] #29 Apply NoRippleTheme on NavigationBar

--- feature/home/src/main/java/com/cmc/curtaincall/feature/home/ui/HomeBottomBar.kt
+++ feature/home/src/main/java/com/cmc/curtaincall/feature/home/ui/HomeBottomBar.kt
@@ -1,15 +1,26 @@
 package com.cmc.curtaincall.feature.home.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material.ripple.RippleAlpha
+import androidx.compose.material.ripple.RippleTheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.cmc.curtaincall.common.design.theme.Cetacean_Blue
+import com.cmc.curtaincall.common.design.theme.White
+import com.cmc.curtaincall.common.design.theme.gmarketsans
 import com.cmc.curtaincall.core.base.BottomDestination
 import com.cmc.curtaincall.core.base.CurtainCallDestination
 import com.cmc.curtaincall.feature.home.HomeDestination
@@ -17,6 +28,15 @@ import com.cmc.curtaincall.feature.livetalk.LiveTalkDestination
 import com.cmc.curtaincall.feature.mypage.MyPageDestination
 import com.cmc.curtaincall.feature.partymember.PartyMemberDestination
 import com.cmc.curtaincall.feature.performance.PerformanceDestination
+
+private object NoRippleTheme : RippleTheme {
+
+    @Composable
+    override fun defaultColor(): Color = Color.Transparent
+
+    @Composable
+    override fun rippleAlpha(): RippleAlpha = RippleAlpha(0f, 0f, 0f, 0f)
+}
 
 @Composable
 fun HomeBottomBar(navHostController: NavHostController) {
@@ -33,13 +53,19 @@ fun HomeBottomBar(navHostController: NavHostController) {
     val hasBottomNavigation = bottomDestinations.any { it.route == currentDestination?.route }
 
     if (hasBottomNavigation) {
-        NavigationBar {
-            bottomDestinations.forEach { bottomDestination ->
-                HomeBottomBarItem(
-                    bottomDestination = bottomDestination,
-                    currentDestination = currentDestination,
-                    navHostController = navHostController
-                )
+        CompositionLocalProvider(LocalRippleTheme provides NoRippleTheme) {
+            NavigationBar(containerColor = White) {
+                bottomDestinations.forEach { bottomDestination ->
+                    if (bottomDestination.route == LiveTalkDestination.LiveTalk.route) {
+                        Spacer(Modifier.weight(1f))
+                    } else {
+                        HomeBottomBarItem(
+                            bottomDestination = bottomDestination,
+                            currentDestination = currentDestination,
+                            navHostController = navHostController
+                        )
+                    }
+                }
             }
         }
     }
@@ -52,9 +78,39 @@ fun RowScope.HomeBottomBarItem(
     navHostController: NavHostController
 ) {
     require(bottomDestination is BottomDestination)
+
+    val selected = bottomDestination.route == currentDestination?.route
     NavigationBarItem(
-        selected = bottomDestination.route == currentDestination?.route,
+        selected = selected,
         onClick = { navHostController.navigate(bottomDestination.route) },
-        icon = { Icon(painter = painterResource(bottomDestination.icon), contentDescription = null) }
+        icon = {
+            Icon(
+                painter = painterResource(
+                    if (selected) {
+                        bottomDestination.selectIcon
+                    } else {
+                        bottomDestination.icon
+                    }
+                ),
+                contentDescription = null,
+                tint = if (selected) Cetacean_Blue else Cetacean_Blue.copy(alpha = 0.2f)
+            )
+        },
+        modifier = Modifier.background(Color.Transparent),
+        label = {
+            if (currentDestination?.route != LiveTalkDestination.LiveTalk.route) {
+                Text(
+                    text = bottomDestination.label,
+                    color = if (selected) Cetacean_Blue else Cetacean_Blue.copy(alpha = 0.2f),
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = gmarketsans
+                )
+            }
+        },
+        alwaysShowLabel = true,
+        colors = NavigationBarItemDefaults.colors(
+            indicatorColor = White
+        )
     )
 }
