129a6c1db70b8444f57985d7ac3cf32b482ee72f
ows3090 <ows3090@naver.com>
2023-07-15 14:36:09 +0900

    [chore] #30 Prevent dark mode from being turned on

--- common/design/src/main/java/com/cmc/curtaincall/common/design/theme/Theme.kt
+++ common/design/src/main/java/com/cmc/curtaincall/common/design/theme/Theme.kt
@@ -2,12 +2,7 @@ package com.cmc.curtaincall.common.design.theme
 
 import android.app.Activity
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
@@ -39,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun CurtainCallTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
