c18bbaf4c0ad5744874247b4b0b640ca6588db83
ows3090 <ows3090@naver.com>
2023-07-23 13:26:42 +0900

    [feat] #40 Add extension method to change sp to dp in Density file

---
+++ common/design/src/main/java/com/cmc/curtaincall/common/design/extensions/Density.kt
@@ -0,0 +1,10 @@
+package com.cmc.curtaincall.common.design.extensions
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+
+@Composable
+fun Dp.toSp() = with(LocalDensity.current) {
+    this@toSp.toSp()
+}
