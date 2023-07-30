e80d37af7d40df03f3e6ff190c0ee270a55a96f9
ows3090 <ows3090@naver.com>
2023-07-30 15:31:25 +0900

    [feat] #47 Develop PerformanceLostItemScreen Body UI

--- feature/performance/src/main/java/com/cmc/curtaincall/feature/performance/ui/lostitem/PerformanceLostItemScreen.kt
+++ feature/performance/src/main/java/com/cmc/curtaincall/feature/performance/ui/lostitem/PerformanceLostItemScreen.kt
@@ -1,21 +1,25 @@
 package com.cmc.curtaincall.feature.performance.ui.lostitem
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import com.cmc.curtaincall.common.design.R
 import com.cmc.curtaincall.common.design.component.SelectedDateCalender
 import com.cmc.curtaincall.common.design.component.TopAppBarWithBack
@@ -79,7 +83,9 @@ private fun PerformanceLostItemContent(
     var isClickedType by remember { mutableStateOf(false) }
     Box(modifier) {
         DropDownLostItem(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .zIndex(if (isClickedDate or isClickedType) 1f else 0f)
+                .fillMaxWidth(),
             isClickedDate = isClickedDate,
             isClickedType = isClickedType,
             onClickDate = { isClickedDate = it },
@@ -90,7 +96,6 @@ private fun PerformanceLostItemContent(
                         modifier = Modifier.padding(top = 10.dp)
                     )
                 }
-
                 if (isClickedType) {
                     LostItemTypeGrid(
                         modifier = Modifier
@@ -101,6 +106,68 @@ private fun PerformanceLostItemContent(
                     )
                 }
             }
+        )
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier.padding(top = 58.dp)
+        ) {
+            itemsIndexed(List(9) { it }) { index, item ->
+                LostItemContent(
+                    modifier = Modifier
+                        .padding(start = if (index % 2 == 1) 6.dp else 0.dp)
+                        .padding(end = if (index % 2 == 0) 6.dp else 0.dp)
+                        .padding(bottom = 18.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun LostItemContent(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .background(Cultured, RoundedCornerShape(10.dp))
+            .padding(horizontal = 8.dp)
+            .padding(top = 8.dp, bottom = 15.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.img_poster),
+            contentDescription = null,
+            modifier = Modifier
+                .aspectRatio(1f)
+                .clip(RoundedCornerShape(10.dp)),
+            contentScale = ContentScale.FillWidth
+        )
+        Text(
+            text = "아이폰 핑크",
+            modifier = Modifier.padding(top = 16.dp),
+            color = Chinese_Black,
+            fontSize = 16.dp.toSp(),
+            fontWeight = FontWeight.Bold,
+            fontFamily = spoqahansanseeo
+        )
+        Text(
+            text = "습득 장소 | LG 아트센터 서울",
+            modifier = Modifier.padding(top = 12.dp),
+            color = Nero,
+            fontSize = 12.dp.toSp(),
+            fontWeight = FontWeight.Medium,
+            fontFamily = spoqahansanseeo,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1
+        )
+        Text(
+            text = "습득 일자 | 2023.7.15",
+            modifier = Modifier.padding(top = 4.dp),
+            color = Nero,
+            fontSize = 12.dp.toSp(),
+            fontWeight = FontWeight.Medium,
+            fontFamily = spoqahansanseeo,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1
         )
     }
 }
