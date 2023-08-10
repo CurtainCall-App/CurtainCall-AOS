ce8d7d13fab2154d875526f4643061fa51ec1ad0
ows3090 <ows3090@naver.com>
2023-08-11 02:45:10 +0900

    [feat] #76 Develop Profile design in MyPageProfileEditContent

--- feature/mypage/src/main/java/com/cmc/curtaincall/feature/mypage/ui/MyPageProfileEditScreen.kt
+++ feature/mypage/src/main/java/com/cmc/curtaincall/feature/mypage/ui/MyPageProfileEditScreen.kt
@@ -1,9 +1,98 @@
 package com.cmc.curtaincall.feature.mypage.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.cmc.curtaincall.common.design.component.TopAppBarWithBack
+import com.cmc.curtaincall.common.design.R
+import com.cmc.curtaincall.common.design.theme.Cetacean_Blue
+import com.cmc.curtaincall.common.design.theme.Nero
+import com.cmc.curtaincall.common.design.theme.White
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun MyPageProfileEditScreen(
     onBack: () -> Unit
 ) {
+    Scaffold(
+        topBar = {
+            TopAppBarWithBack(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(54.dp),
+                title = stringResource(R.string.mypage_profile_edit_appbar_title),
+                containerColor = White,
+                contentColor = Nero,
+                onClick = onBack
+            )
+        }
+    ) { paddingValues ->
+        MyPageProfileEditContent(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+                .background(White)
+        )
+    }
+}
+
+@Composable
+private fun MyPageProfileEditContent(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(top = 75.dp, bottom = 50.dp)
+                .size(92.dp)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.img_example_detail),
+                contentDescription = null,
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+                    .size(80.dp)
+                    .clip(RoundedCornerShape(27.dp)),
+                contentScale = ContentScale.FillBounds
+            )
+            IconButton(
+                onClick = { },
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .size(32.dp)
+                    .background(Cetacean_Blue, CircleShape)
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_photo_library),
+                    contentDescription = null,
+                    modifier = Modifier.size(18.dp),
+                    tint = Color.Unspecified
+                )
+            }
+        }
+    }
 }
