e0805ee696c5d241756906cc508be910127a2743
ows3090 <ows3090@naver.com>
2023-08-10 21:00:40 +0900

    [feat] #75 Create MyPageProfile Composable for MyPageContent

--- feature/mypage/src/main/java/com/cmc/curtaincall/feature/mypage/ui/MyPageScreen.kt
+++ feature/mypage/src/main/java/com/cmc/curtaincall/feature/mypage/ui/MyPageScreen.kt
@@ -1,17 +1,46 @@
 package com.cmc.curtaincall.feature.mypage.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.cmc.curtaincall.common.design.component.TopAppBarOnlyAction
 import com.cmc.curtaincall.common.design.theme.Nero
 import com.cmc.curtaincall.common.design.theme.White
+import com.cmc.curtaincall.common.design.R
+import com.cmc.curtaincall.common.design.extensions.toSp
+import com.cmc.curtaincall.common.design.theme.Black
+import com.cmc.curtaincall.common.design.theme.Cetacean_Blue
+import com.cmc.curtaincall.common.design.theme.Cultured
+import com.cmc.curtaincall.common.design.theme.Me_Pink
+import com.cmc.curtaincall.common.design.theme.Silver_Sand
+import com.cmc.curtaincall.common.design.theme.spoqahansanseeo
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -48,4 +77,127 @@ fun MyPageScreen(
 private fun MyPageContent(
     modifier: Modifier = Modifier
 ) {
+    val verticalScrollState = rememberScrollState()
+    Column(modifier.verticalScroll(verticalScrollState)) {
+        MyPageProfile(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+                .padding(top = 6.dp)
+        )
+    }
+}
+
+@Composable
+private fun MyPageProfile(
+    modifier: Modifier = Modifier
+) {
+    Column(modifier) {
+        Text(
+            text = stringResource(R.string.mypage_title),
+            color = Black,
+            fontSize = 24.dp.toSp(),
+            fontWeight = FontWeight.Bold,
+            fontFamily = spoqahansanseeo
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 40.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Column {
+                Box(Modifier.size(92.dp)) {
+                    Image(
+                        painter = painterResource(R.drawable.img_example_detail),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .align(Alignment.TopStart)
+                            .size(80.dp)
+                            .clip(RoundedCornerShape(27.dp)),
+                        contentScale = ContentScale.FillBounds
+                    )
+                    IconButton(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier
+                            .align(Alignment.BottomEnd)
+                            .size(32.dp)
+                            .background(Cetacean_Blue, CircleShape)
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_pen),
+                            contentDescription = null,
+                            modifier = Modifier.size(18.dp),
+                            tint = Color.Unspecified
+                        )
+                    }
+                }
+                Text(
+                    text = "고라파덕님",
+                    modifier = Modifier.padding(top = 10.dp),
+                    color = Black,
+                    fontSize = 18.dp.toSp(),
+                    fontWeight = FontWeight.SemiBold,
+                    fontFamily = spoqahansanseeo
+                )
+            }
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Cultured, RoundedCornerShape(15.dp)),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(vertical = 18.dp)
+                    .weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(R.string.mypage_my_gathering_tab),
+                    color = Nero,
+                    fontSize = 16.dp.toSp(),
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = spoqahansanseeo
+                )
+                Text(
+                    text = "2",
+                    modifier = Modifier.padding(top = 4.dp),
+                    color = Me_Pink,
+                    fontSize = 24.dp.toSp(),
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = spoqahansanseeo
+                )
+            }
+            Spacer(
+                modifier = Modifier
+                    .background(Silver_Sand)
+                    .size(1.dp, 30.dp)
+
+            )
+            Column(
+                modifier = Modifier
+                    .padding(vertical = 18.dp)
+                    .weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(R.string.mypage_my_participation_tab),
+                    color = Nero,
+                    fontSize = 16.dp.toSp(),
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = spoqahansanseeo
+                )
+                Text(
+                    text = "2",
+                    modifier = Modifier.padding(top = 4.dp),
+                    color = Me_Pink,
+                    fontSize = 24.dp.toSp(),
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = spoqahansanseeo
+                )
+            }
+        }
+    }
 }
