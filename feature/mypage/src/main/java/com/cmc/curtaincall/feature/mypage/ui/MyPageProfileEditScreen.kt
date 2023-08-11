f9b449a1c14a9982cdc2866c46624ba5590629d9
ows3090 <ows3090@naver.com>
2023-08-12 00:44:55 +0900

    [feat] #76 Create MyPageNickName Composable and Complete MyPageProfileEditContent

--- feature/mypage/src/main/java/com/cmc/curtaincall/feature/mypage/ui/MyPageProfileEditScreen.kt
+++ feature/mypage/src/main/java/com/cmc/curtaincall/feature/mypage/ui/MyPageProfileEditScreen.kt
@@ -2,8 +2,10 @@ package com.cmc.curtaincall.feature.mypage.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -11,11 +13,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -23,12 +33,19 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.cmc.curtaincall.common.design.component.TopAppBarWithBack
 import com.cmc.curtaincall.common.design.R
+import com.cmc.curtaincall.common.design.extensions.toSp
 import com.cmc.curtaincall.common.design.theme.Cetacean_Blue
+import com.cmc.curtaincall.common.design.theme.Cultured
+import com.cmc.curtaincall.common.design.theme.Me_Pink
 import com.cmc.curtaincall.common.design.theme.Nero
+import com.cmc.curtaincall.common.design.theme.Roman_Silver
 import com.cmc.curtaincall.common.design.theme.White
+import com.cmc.curtaincall.common.design.theme.spoqahansanseeo
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -54,6 +71,59 @@ internal fun MyPageProfileEditScreen(
                 .fillMaxSize()
                 .background(White)
         )
+    }
+}
+
+@Composable
+private fun MyPageNickName(
+    modifier: Modifier = Modifier,
+    onDuplicateCheck: (String) -> Unit = {}
+) {
+    var nickNameState by remember { mutableStateOf("") }
+    Column(modifier) {
+        BasicTextField(
+            value = nickNameState,
+            onValueChange = { nickNameState = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+                .background(Cultured, RoundedCornerShape(10.dp)),
+            textStyle = TextStyle(
+                color = Roman_Silver,
+                fontSize = 16.dp.toSp(),
+                fontWeight = FontWeight.Medium,
+                fontFamily = spoqahansanseeo
+            ),
+            singleLine = true,
+            maxLines = 1,
+            decorationBox = { innerTextField ->
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(20.dp),
+                    contentAlignment = Alignment.CenterStart
+                ) {
+                    innerTextField()
+                }
+            }
+        )
+        Box(
+            modifier = Modifier
+                .align(Alignment.End)
+                .padding(top = 16.dp)
+                .background(Me_Pink, RoundedCornerShape(8.dp))
+                .padding(horizontal = 20.dp, vertical = 8.dp)
+                .clickable { onDuplicateCheck(nickNameState) },
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = stringResource(R.string.mypage_nickname_duplicate_check),
+                color = White,
+                fontSize = 13.dp.toSp(),
+                fontWeight = FontWeight.Medium,
+                fontFamily = spoqahansanseeo
+            )
+        }
     }
 }
 
@@ -93,6 +163,30 @@ private fun MyPageProfileEditContent(
                     tint = Color.Unspecified
                 )
             }
+        }
+        MyPageNickName(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+        )
+        Spacer(Modifier.weight(1f))
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+                .padding(bottom = 19.dp)
+                .height(52.dp),
+            shape = RoundedCornerShape(12.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Me_Pink)
+        ) {
+            Text(
+                text = stringResource(R.string.mypage_nickname_change_complete),
+                color = White,
+                fontSize = 16.dp.toSp(),
+                fontWeight = FontWeight.Medium,
+                fontFamily = spoqahansanseeo
+            )
         }
     }
 }
