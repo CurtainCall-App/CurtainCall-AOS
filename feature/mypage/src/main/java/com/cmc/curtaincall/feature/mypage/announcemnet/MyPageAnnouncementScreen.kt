6b3926afc48fc604cd88e69331b0fc9acfc262f6
ows3090 <ows3090@naver.com>
2023-08-13 02:15:27 +0900

    [feat] #93 Develop MyPageAnnouncementContent composable

--- feature/mypage/src/main/java/com/cmc/curtaincall/feature/mypage/announcemnet/MyPageAnnouncementScreen.kt
+++ feature/mypage/src/main/java/com/cmc/curtaincall/feature/mypage/announcemnet/MyPageAnnouncementScreen.kt
@@ -1,20 +1,37 @@
 package com.cmc.curtaincall.feature.mypage.announcemnet
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.cmc.curtaincall.common.design.component.TopAppBarWithBack
 import com.cmc.curtaincall.common.design.theme.Nero
 import com.cmc.curtaincall.common.design.theme.White
 import com.cmc.curtaincall.common.design.R
+import com.cmc.curtaincall.common.design.extensions.toSp
+import com.cmc.curtaincall.common.design.theme.Bright_Gray
+import com.cmc.curtaincall.common.design.theme.Chinese_Black
+import com.cmc.curtaincall.common.design.theme.Roman_Silver
+import com.cmc.curtaincall.common.design.theme.spoqahansanseeo
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -47,4 +64,67 @@ internal fun MyPageAnnouncementScreen(
 private fun MyPageAnnouncementContent(
     modifier: Modifier = Modifier
 ) {
+    val news = listOf(true, true, false, false)
+    Column(modifier) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp)
+        ) {
+            itemsIndexed(news) { index, item ->
+                AnnouncementItem(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp),
+                    title = "커튼콜 서비스 개편 안내",
+                    date = "2023.6.17",
+                    isNew = news[index]
+                )
+                Spacer(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(1.dp)
+                        .background(Bright_Gray)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun AnnouncementItem(
+    modifier: Modifier = Modifier,
+    title: String,
+    date: String,
+    isNew: Boolean = false
+) {
+    Column(modifier.padding(vertical = 20.dp)) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = title,
+                color = Chinese_Black,
+                fontSize = 16.dp.toSp(),
+                fontWeight = FontWeight.Bold,
+                fontFamily = spoqahansanseeo
+            )
+            if (isNew) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_new),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(start = 6.dp)
+                        .size(16.dp),
+                    tint = Color.Unspecified
+                )
+            }
+        }
+        Text(
+            text = date,
+            modifier = Modifier.padding(top = 8.dp),
+            color = Roman_Silver,
+            fontSize = 13.dp.toSp(),
+            fontFamily = spoqahansanseeo,
+            fontWeight = FontWeight.Medium
+        )
+    }
 }
