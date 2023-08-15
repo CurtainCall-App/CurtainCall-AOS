e1ce9b6a351909d01ba98be735ab738c3302d323
ows3090 <ows3090@naver.com>
2023-08-15 16:11:14 +0900

    [feat] #100 Change WelComeScreen UI

--- feature/auth/src/main/java/com/cmc/curtaincall/feature/auth/welcome/WelComeScreen.kt
+++ feature/auth/src/main/java/com/cmc/curtaincall/feature/auth/welcome/WelComeScreen.kt
@@ -2,10 +2,13 @@ package com.cmc.curtaincall.feature.auth.welcome
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,58 +18,56 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.cmc.curtaincall.common.design.R
-import com.cmc.curtaincall.common.design.theme.*
+import com.cmc.curtaincall.common.design.component.CurtainCallRoundedTextButton
+import com.cmc.curtaincall.common.design.extensions.toSp
+import com.cmc.curtaincall.common.design.theme.Cetacean_Blue
+import com.cmc.curtaincall.common.design.theme.Me_Pink
+import com.cmc.curtaincall.common.design.theme.White
+import com.cmc.curtaincall.common.design.theme.spoqahansanseeo
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
 internal fun WelComeScreen(
     onNavigateHome: () -> Unit
 ) {
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setStatusBarColor(Cetacean_Blue)
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(White),
+            .background(Cetacean_Blue),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Spacer(Modifier.weight(232f))
         Image(
             painter = painterResource(R.drawable.ic_welcome_anim),
             contentDescription = null,
-            modifier = Modifier
-                .padding(top = 201.dp)
-                .size(100.dp)
+            modifier = Modifier.size(100.dp)
         )
-
         Text(
             text = stringResource(R.string.signup_welcome_description),
-            modifier = Modifier.padding(top = 13.dp),
-            color = Gunmetal,
-            fontSize = 24.sp,
+            modifier = Modifier.padding(top = 14.dp),
+            color = Me_Pink,
+            fontSize = 20.dp.toSp(),
             fontWeight = FontWeight.Bold,
             fontFamily = spoqahansanseeo,
-            lineHeight = 32.sp,
+            lineHeight = 32.dp.toSp(),
             textAlign = TextAlign.Center
         )
-
-        Spacer(Modifier.weight(1f))
-
-        Button(
+        Spacer(Modifier.weight(259f))
+        CurtainCallRoundedTextButton(
             onClick = onNavigateHome,
-            modifier = Modifier.fillMaxWidth()
-                .padding(bottom = 15.dp)
-                .padding(horizontal = 22.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 19.dp)
+                .padding(horizontal = 20.dp)
                 .height(52.dp),
-            shape = RoundedCornerShape(12.dp),
-            colors = ButtonDefaults.buttonColors(containerColor = Me_Pink)
-        ) {
-            Text(
-                text = stringResource(R.string.signup_welcome_start),
-                color = White,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Medium,
-                fontFamily = spoqahansanseeo,
-                textAlign = TextAlign.Center
-            )
-        }
+            title = stringResource(R.string.signup_welcome_start),
+            fontSize = 16.dp.toSp(),
+            containerColor = Me_Pink,
+            contentColor = White
+        )
     }
 }
