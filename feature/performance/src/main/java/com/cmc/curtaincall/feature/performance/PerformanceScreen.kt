7996ed5f67f18d07c638c92a50f35df895ff940a
ows3090 <ows3090@naver.com>
2023-08-16 10:42:39 +0900

    [feat] #105 Create PerformanceDetailCard component

--- feature/performance/src/main/java/com/cmc/curtaincall/feature/performance/PerformanceScreen.kt
+++ feature/performance/src/main/java/com/cmc/curtaincall/feature/performance/PerformanceScreen.kt
@@ -1,29 +1,25 @@
 package com.cmc.curtaincall.feature.performance
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.cmc.curtaincall.common.design.R
 import com.cmc.curtaincall.common.design.component.CurtainCallSelectTypeButton
 import com.cmc.curtaincall.common.design.component.SearchAppBar
 import com.cmc.curtaincall.common.design.component.TopAppBarOnlySearch
+import com.cmc.curtaincall.common.design.component.content.card.PerformanceDetailCard
 import com.cmc.curtaincall.common.design.extensions.toSp
 import com.cmc.curtaincall.common.design.theme.*
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
@@ -115,10 +111,19 @@ private fun PerformanceContent(
             }
 
             itemsIndexed(Array(10) {}) { index, item ->
-                PerformanceCard(
+                PerformanceDetailCard(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .clickable { onNavigateDetail() }
+                        .padding(vertical = 12.dp)
+                        .clickable { onNavigateDetail() },
+                    painter = painterResource(R.drawable.img_poster),
+                    title = "드림하이",
+                    rate = 4.89f,
+                    numberOfTotal = 324,
+                    period = "2023.6.1 - 2023.6.18",
+                    runningTime = "200분",
+                    dates = listOf("화-금 19:00", "토,일 14:00, 19:00"),
+                    location = "LG아트센터 서울"
                 )
                 if (index != 9) {
                     Spacer(
@@ -129,173 +134,6 @@ private fun PerformanceContent(
                             .background(Cultured)
                     )
                 }
-            }
-        }
-    }
-}
-
-@Composable
-private fun PerformanceCard(
-    modifier: Modifier = Modifier
-) {
-    var selectBookmark by remember { mutableStateOf(false) }
-    Row(modifier = modifier.padding(vertical = 12.dp)) {
-        Image(
-            painter = painterResource(R.drawable.img_poster),
-            contentDescription = null,
-            modifier = Modifier.size(114.dp, 153.dp)
-        )
-
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .padding(start = 14.dp)
-        ) {
-            Row {
-                Column {
-                    Text(
-                        text = "드림하이",
-                        color = Chinese_Black,
-                        fontSize = 16.dp.toSp(),
-                        fontWeight = FontWeight.Bold,
-                        fontFamily = spoqahansanseeo
-                    )
-
-                    Row(
-                        modifier = Modifier.padding(top = 3.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Icon(
-                            painter = painterResource(R.drawable.ic_star),
-                            contentDescription = null,
-                            modifier = Modifier.size(16.dp),
-                            tint = Color.Unspecified
-                        )
-                        Text(
-                            text = "4.8",
-                            modifier = Modifier.padding(start = 2.dp),
-                            color = Arsenic,
-                            fontSize = 14.dp.toSp(),
-                            fontWeight = FontWeight.Medium,
-                            fontFamily = spoqahansanseeo,
-                            lineHeight = 22.sp
-                        )
-                        Text(
-                            text = "(324)",
-                            modifier = Modifier.padding(start = 2.dp),
-                            color = Roman_Silver,
-                            fontSize = 14.dp.toSp(),
-                            fontWeight = FontWeight.Medium,
-                            fontFamily = spoqahansanseeo,
-                            lineHeight = 22.sp
-                        )
-                    }
-                }
-                Spacer(Modifier.weight(1f))
-                IconButton(
-                    onClick = { selectBookmark = selectBookmark.not() },
-                    modifier = Modifier
-                        .clip(CircleShape)
-                        .size(26.dp),
-                    colors = IconButtonDefaults.iconButtonColors(
-                        containerColor = if (selectBookmark) Cetacean_Blue else Bright_Gray
-                    )
-                ) {
-                    Icon(
-                        painter = painterResource(if (selectBookmark) R.drawable.ic_bookmark_sel else R.drawable.ic_bookmark),
-                        contentDescription = null,
-                        tint = Color.Unspecified
-                    )
-                }
-            }
-
-            Row(
-                modifier = Modifier.padding(top = 10.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = "기간",
-                    color = Roman_Silver,
-                    fontSize = 14.dp.toSp(),
-                    fontWeight = FontWeight.Medium,
-                    fontFamily = spoqahansanseeo,
-                    lineHeight = 22.sp
-                )
-                Text(
-                    text = "2023.6.1 - 2023.6.18",
-                    modifier = Modifier.padding(start = 13.dp),
-                    color = Nero,
-                    fontSize = 14.dp.toSp(),
-                    fontWeight = FontWeight.Medium,
-                    fontFamily = spoqahansanseeo,
-                    lineHeight = 22.sp
-                )
-            }
-
-            Row(
-                modifier = Modifier.padding(top = 4.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = "시간",
-                    color = Roman_Silver,
-                    fontSize = 14.dp.toSp(),
-                    fontWeight = FontWeight.Medium,
-                    fontFamily = spoqahansanseeo,
-                    lineHeight = 22.sp
-                )
-                Text(
-                    text = "200분",
-                    modifier = Modifier.padding(start = 13.dp),
-                    color = Nero,
-                    fontSize = 14.dp.toSp(),
-                    fontWeight = FontWeight.Medium,
-                    fontFamily = spoqahansanseeo,
-                    lineHeight = 22.sp
-                )
-            }
-
-            Row(modifier = Modifier.padding(top = 4.dp)) {
-                Text(
-                    text = "일정",
-                    color = Roman_Silver,
-                    fontSize = 14.dp.toSp(),
-                    fontWeight = FontWeight.Medium,
-                    fontFamily = spoqahansanseeo,
-                    lineHeight = 22.sp
-                )
-                Text(
-                    text = "화-금 19:00\n토, 일 14:00, 19:00",
-                    modifier = Modifier.padding(start = 13.dp),
-                    color = Nero,
-                    fontSize = 14.dp.toSp(),
-                    fontWeight = FontWeight.Medium,
-                    fontFamily = spoqahansanseeo,
-                    lineHeight = 22.dp.toSp()
-                )
-            }
-
-            Row(
-                modifier = Modifier.padding(top = 4.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = "장소",
-                    color = Roman_Silver,
-                    fontSize = 14.dp.toSp(),
-                    fontWeight = FontWeight.Medium,
-                    fontFamily = spoqahansanseeo,
-                    lineHeight = 22.sp
-                )
-                Text(
-                    text = "LG아트센터 서울",
-                    modifier = Modifier.padding(start = 13.dp),
-                    color = Nero,
-                    fontSize = 14.dp.toSp(),
-                    fontWeight = FontWeight.Medium,
-                    fontFamily = spoqahansanseeo,
-                    lineHeight = 22.sp
-                )
             }
         }
     }
@@ -326,106 +164,5 @@ private fun SortTypeRow(
                 .size(14.dp, 11.dp),
             tint = Color.Unspecified
         )
-    }
-}
-
-@Composable
-private fun SelectSortButton(
-    modifier: Modifier = Modifier
-) {
-    var sortType by remember { mutableStateOf(true) }
-    Row(
-        modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Spacer(
-            modifier = Modifier
-                .size(4.dp)
-                .background(
-                    if (sortType) Fiery_Rose else Color.Transparent,
-                    CircleShape
-                )
-                .clickable { sortType = true }
-        )
-        Text(
-            text = stringResource(R.string.performance_search_popular_sort),
-            modifier = Modifier.padding(start = 4.dp),
-            color = if (sortType) Nero else Silver_Sand,
-            fontSize = 12.dp.toSp(),
-            fontWeight = FontWeight.Medium,
-            fontFamily = spoqahansanseeo
-        )
-
-        Spacer(
-            modifier = Modifier
-                .padding(start = 10.dp)
-                .size(4.dp)
-                .background(
-                    if (sortType.not()) Fiery_Rose else Color.Transparent,
-                    CircleShape
-                )
-                .clickable { sortType = false }
-        )
-        Text(
-            text = stringResource(R.string.performance_search_korean_sort),
-            modifier = Modifier.padding(start = 4.dp),
-            color = if (sortType.not()) Nero else Silver_Sand,
-            fontSize = 12.dp.toSp(),
-            fontWeight = FontWeight.Medium,
-            fontFamily = spoqahansanseeo
-        )
-    }
-}
-
-@Composable
-private fun SelectTypeButton(
-    modifier: Modifier = Modifier
-) {
-    var selectType by remember { mutableStateOf(true) }
-    Row(
-        modifier = modifier.background(Cultured, RoundedCornerShape(10.dp)),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Box(
-            modifier = Modifier
-                .weight(1f)
-                .padding(start = 3.dp)
-                .height(39.dp)
-                .background(
-                    if (selectType) Me_Pink else Cultured,
-                    RoundedCornerShape(8.dp)
-                )
-                .clickable { selectType = true },
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = stringResource(R.string.partymember_create_classification_theater),
-                color = if (selectType) White else Silver_Sand,
-                fontSize = 16.dp.toSp(),
-                fontWeight = FontWeight.Bold,
-                fontFamily = spoqahansanseeo
-            )
-        }
-
-        Box(
-            modifier = Modifier
-                .weight(1f)
-                .padding(end = 3.dp)
-                .height(39.dp)
-                .background(
-                    if (selectType.not()) Me_Pink else Cultured,
-                    RoundedCornerShape(8.dp)
-                )
-                .clickable { selectType = false },
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = stringResource(R.string.partymember_create_classification_musical),
-                color = if (selectType.not()) White else Silver_Sand,
-                fontSize = 16.dp.toSp(),
-                fontWeight = FontWeight.Bold,
-                fontFamily = spoqahansanseeo
-            )
-        }
     }
 }
