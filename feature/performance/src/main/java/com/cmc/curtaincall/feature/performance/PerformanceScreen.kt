87acdcf7ea7970856f21b60e930ca57f2648bb7c
ows3090 <ows3090@naver.com>
2023-08-16 21:41:49 +0900

    [build] #105 Remove context variable bacause of detekt

--- feature/performance/src/main/java/com/cmc/curtaincall/feature/performance/PerformanceScreen.kt
+++ feature/performance/src/main/java/com/cmc/curtaincall/feature/performance/PerformanceScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -75,7 +74,6 @@ private fun PerformanceContent(
     modifier: Modifier = Modifier,
     onNavigateDetail: () -> Unit
 ) {
-    val context = LocalContext.current
     var isCheckFirstType by remember { mutableStateOf(true) }
     var sortType by remember { mutableStateOf(SortType.STAR) }
     var showDialog by remember { mutableStateOf(false) }
