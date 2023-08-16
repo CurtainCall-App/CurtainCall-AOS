f034b25130d09bca80026e59723b2445b7a82d15
ows3090 <ows3090@naver.com>
2023-08-16 20:32:19 +0900

    [feat] #101 Create NetworkModule for Retrofit and OkHttpClient DI

--- core/network/src/main/java/com/cmc/curtaincall/core/network/di/NetworkModule.kt
+++ core/network/src/main/java/com/cmc/curtaincall/core/network/di/NetworkModule.kt
@@ -1,10 +1,12 @@
 package com.cmc.curtaincall.core.network.di
 
 import com.cmc.curtaincall.core.network.BuildConfig
+import com.cmc.curtaincall.core.network.interceptors.CurtainCallInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -16,9 +18,21 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit =
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient
+    ): Retrofit =
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideClient(
+        curtainCallInterceptor: CurtainCallInterceptor
+    ): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(curtainCallInterceptor)
             .build()
 }
