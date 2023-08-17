cdcee35dab91d8c8e55ae06b3ad28c39311912c2
ows3090 <ows3090@naver.com>
2023-08-17 22:03:19 +0900

    [feat] #112 Create CurtainCallDropDownButton component in design system

--- common/design/src/main/java/com/cmc/curtaincall/common/design/component/basic/Button.kt
+++ common/design/src/main/java/com/cmc/curtaincall/common/design/component/basic/Button.kt
@@ -2,9 +2,11 @@ package com.cmc.curtaincall.common.design.component.basic
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -12,11 +14,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -136,6 +140,44 @@ fun CurtainCallSelectTypeButton(
                 fontFamily = spoqahansanseeo
             )
         }
+    }
+}
+
+@Composable
+fun CurtainCallDropDownButton(
+    modifier: Modifier = Modifier,
+    isClicked: Boolean = false,
+    title: String,
+    fontSize: TextUnit,
+    containerColor: Color,
+    contentColor: Color,
+    borderColor: Color,
+    radiusSize: Dp,
+    contentModifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .background(containerColor, RoundedCornerShape(radiusSize))
+            .border(BorderStroke(1.dp, if (isClicked) borderColor else Color.Transparent), RoundedCornerShape(radiusSize))
+            .then(contentModifier),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.weight(1f),
+            color = contentColor,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Medium,
+            fontFamily = spoqahansanseeo
+        )
+        Icon(
+            painter = painterResource(R.drawable.ic_dropdown),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxHeight()
+                .aspectRatio(1f),
+            tint = Color.Unspecified
+        )
     }
 }
 
