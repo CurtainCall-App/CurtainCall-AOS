d903a2882e87ede2d32295511ceb5f7a85322cb3
ows3090 <ows3090@naver.com>
2023-08-19 17:50:38 +0900

    [feat] #118 Create DataStoreQualifier to inject difference dataStore

---
+++ core/local/src/main/java/com/cmc/curtaincall/core/local/qualifiers/DataStoreQualifier.kt
@@ -0,0 +1,11 @@
+package com.cmc.curtaincall.core.local.qualifiers
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class TokenDataStore
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class MemberDataStore
