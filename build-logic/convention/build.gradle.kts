4924feeb71752a75008f80122baac2ea02e13870
ows3090 <ows3090@naver.com>
2023-11-24 15:12:41 +0900

    [build] #175 Add build-logic module to apply gradle convention plugin

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    `kotlin-dsl`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.android.tools.common)
+    compileOnly(libs.kotlin.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = "curtaincall.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+    }
+}
