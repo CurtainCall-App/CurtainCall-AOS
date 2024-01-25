c03252cfe0419975e8d395d2529b7014e8ca7f4a
ows3090 <ows3090@naver.com>
2024-01-26 03:19:21 +0900

    [feat] #216 Add dependency related to naver login

--- app/src/main/java/com/cmc/curtaincall/CurtainCallApplication.kt
+++ app/src/main/java/com/cmc/curtaincall/CurtainCallApplication.kt
@@ -2,8 +2,11 @@ package com.cmc.curtaincall
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
+import com.navercorp.nid.NaverIdLoginSDK
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+
+private const val ClientName = "CurtainCall"
 
 @HiltAndroidApp
 class CurtainCallApplication : Application() {
@@ -11,5 +14,6 @@ class CurtainCallApplication : Application() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
         KakaoSdk.init(this, BuildConfig.KAKAO_APP_KEY)
+        NaverIdLoginSDK.initialize(this, BuildConfig.NAVER_CLIENT_ID, BuildConfig.NAVER_CLIENT_SECRET, ClientName)
     }
 }
