b49b8abe8be263f989eaa7972afe95697bbe6fc8
ows3090 <ows3090@naver.com>
2024-01-27 13:38:45 +0900

    [feat] #216 Create ShowRecommendation Model related to api

---
+++ domain/src/main/java/com/cmc/curtaincall/domain/model/show/ShowRecommendationModel.kt
@@ -0,0 +1,11 @@
+package com.cmc.curtaincall.domain.model.show
+
+data class ShowRecommendationModel(
+    val id: Int = Int.MIN_VALUE,
+    val description: String = "",
+    val showId: String = "",
+    val name: String = "",
+    val genre: String = "",
+    val startDate: String = "",
+    val endDate: String = ""
+)
