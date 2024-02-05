3f99f38671e325b38c2d71ae19a8aaa037bf0f9e
ows3090 <ows3090@naver.com>
2024-02-06 00:42:36 +0900

    [UI] #220 Create BottomSheet component by using ModalBottomSheet in Material3

---
+++ common/designsystem/src/main/java/com/cmc/curtaincall/common/designsystem/component/sheets/bottom/BottomSheets.kt
@@ -0,0 +1,100 @@
+package com.cmc.curtaincall.common.designsystem.component.sheets.bottom
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.cmc.curtaincall.common.designsystem.R
+import com.cmc.curtaincall.common.designsystem.theme.CurtainCallTheme
+import com.cmc.curtaincall.common.designsystem.theme.Grey5
+import com.cmc.curtaincall.common.designsystem.theme.NoRippleTheme
+import com.cmc.curtaincall.domain.type.ShowSortType
+
+private val ShowSortBottomSheetHeight = 270.dp
+private val ShowSortBottomSheetRadius = 28.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CurtainCallShowSortBottomSheet(
+    showSortType: ShowSortType = ShowSortType.POPULAR,
+    onSelectSortType: (ShowSortType) -> Unit = {},
+    onDismissRequest: () -> Unit = {}
+) {
+    ModalBottomSheet(
+        onDismissRequest = onDismissRequest,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(ShowSortBottomSheetHeight),
+        shape = RoundedCornerShape(
+            topStart = ShowSortBottomSheetRadius,
+            topEnd = ShowSortBottomSheetRadius
+        ),
+        containerColor = CurtainCallTheme.colors.background
+    ) {
+        ShowSortType.values().forEach { sortType ->
+            Row(
+                modifier = Modifier
+                    .padding(start = 30.dp, end = 15.dp)
+                    .fillMaxWidth()
+                    .height(50.dp)
+                    .clickable { onSelectSortType(sortType) },
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = sortType.value,
+                    modifier = Modifier.weight(1f),
+                    style = CurtainCallTheme.typography.body2.copy(
+                        fontWeight = FontWeight.SemiBold,
+                        color = if (sortType == showSortType) {
+                            CurtainCallTheme.colors.primary
+                        } else {
+                            Grey5
+                        }
+                    )
+                )
+                if (sortType == showSortType) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_check),
+                        contentDescription = null,
+                        modifier = Modifier.size(24.dp),
+                        tint = CurtainCallTheme.colors.secondary
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CurtainCallShowSortBottomSheetPreview() {
+    var sortType by remember { mutableStateOf(ShowSortType.POPULAR) }
+    CurtainCallTheme {
+        CompositionLocalProvider(LocalRippleTheme provides NoRippleTheme) {
+            CurtainCallShowSortBottomSheet(
+                showSortType = sortType,
+                onSelectSortType = { sortType = it }
+            )
+        }
+    }
+}
